5d37e450ecbd8c890945440c582ce667896e949d
yuichiro <ym.u.ichiro@gmail.com>
2022-02-14 08:59:06 +0900

    [fix] 非推奨のnew BufferをBuffer.fromへ置き換え

--- src/read.ts
+++ src/read.ts
@@ -7,11 +7,3 @@ const buffer = readFileSync(resolve("resolve.png"));
 const chunks = extract(buffer).filter(e => e.name === "tEXt");
 const values = chunks.map(e => text.decode(e));
 console.log(values);
-
-/*
- PNGヘッダー	8byte	PNGファイルであることを示す
- IHDRチャンク	25byte	PNGイメージの基本情報
- IDATチャンク	Nbyte	画像の実データ
- tEXtチャンク Nbyte 適当な文字列(ここに文字列化したCSSを格納)
- IENDチャンク	12byte	PNGファイルの終端を表す
-*/
